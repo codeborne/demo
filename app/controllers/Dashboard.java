c11da1722129e50914f699e47aa843aae380ad33
Andrei Solntsev <drusha49@hotmail.com>
2012-04-03 17:51:25 +0300

    Normal login process (using login/logout methods). Show dashboard, show user name if logged in.

---
+++ app/controllers/Dashboard.java
@@ -0,0 +1,11 @@
+package controllers;
+
+import play.mvc.Controller;
+
+public class Dashboard extends Controller {
+  public static void index() {
+    String userName = session.get("userName");
+    String personalCode = session.get("personalCode");
+    render(userName, personalCode);
+  }
+}
--- app/controllers/User.java
+++ app/controllers/User.java
@@ -5,12 +5,18 @@ import com.codeborne.security.mobileid.MobileIDAuthenticator;
 import com.codeborne.security.mobileid.MobileIDSession;
 import play.*;
 import play.cache.Cache;
+import play.data.validation.Validation;
 import play.mvc.*;
 
-public class Login extends Controller {
+import java.io.File;
+
+public class User extends Controller {
 
   static MobileIDAuthenticator mid = new MobileIDAuthenticator("https://www.openxades.org:8443/");
 
+  static {
+    System.setProperty("javax.net.ssl.trustStore", new File("keystore.jks").getAbsolutePath());
+  }
   public static void login(String error) {
     render(error);
   }
@@ -25,26 +31,30 @@ public class Login extends Controller {
     } catch (AuthenticationException e) {
       String error = e.getMessage();
       params.flash(); // add http parameters to the flash scope
-      validation.keep(); // keep the errors for the next request
+      Validation.keep(); // keep the errors for the next request
       login(error);
     }
   }
 
-  public static void mid() {
+  public static void completeMid() {
     try {
       MobileIDSession mobileIDSession = MobileIDSession.fromString(session.get("mobileid-session"));
       mid.waitForLogin(mobileIDSession);
-      String firstName = mobileIDSession.firstName;
-      String lastName = mobileIDSession.lastName;
-      String personalCode = mobileIDSession.personalCode;
-      render(firstName, lastName, personalCode);
+      session.put("userName", mobileIDSession.firstName + " " + mobileIDSession.lastName);
+      session.put("personalCode", mobileIDSession.personalCode);
+      Dashboard.index();
       session.remove("mobileid-session");
     } catch (AuthenticationException e) {
       String error = e.getMessage();
       params.flash(); // add http parameters to the flash scope
-      validation.keep(); // keep the errors for the next request
+      Validation.keep(); // keep the errors for the next request
       login(error);
     }
   }
 
+  public static void logout() {
+    session.remove("userName");
+    session.remove("personalCode");
+    Dashboard.index();
+  }
 }
